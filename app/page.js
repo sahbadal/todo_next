fca3c96213aa35f82abb841d977b62f1316f8734
badalsah <badalsah8541@gmail.com>
2025-09-16 18:56:13 +0530

    todo structure created

--- app/page.js
+++ app/page.js
@@ -1,7 +1,54 @@
+import Todo from "@/components/Todo";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello there</h1>
+      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Title"
+          className="py-2 px-3 border-2 w-full"
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Description"
+          className="py-2 px-3 border-2 w-full"
+        ></textarea>
+        <button
+          type="submit"
+          className="bg-blue-600 py-3 px-11 text-white"
+        >
+          Add Todo
+        </button>
+      </form>
+
+      <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
+        <table className="w-full text-sm text-left text-gray-700">
+          <thead className="text-xs uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Id
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <Todo />
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
---
+++ components/Todo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Todo = () => {
+    return (
+        <tr className="bg-white border-b border-gray-200">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                1
+            </th>
+            <td className="px-6 py-4">Study</td>
+            <td className="px-6 py-4">Learn Next js</td>
+            <td className="px-6 py-4">Pending</td>
+            <td className="px-6 py-4 flex gap-1">
+                <button className='py-2 px-4 bg-red-500 text-white'>Delete</button>
+                <button className='py-2 px-4 bg-green-500 text-white'>Done</button>
+            </td>
+        </tr>
+    )
+}
+
+export default Todo
